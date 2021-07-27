d05c029b10ac2a80b8754ecfb6264d88e3e9a6dc
Fadhil Abbas <79476134+fadhil-code@users.noreply.github.com>
2021-07-27 14:38:14 +0300

    add MachineLearning

---
+++ MachineLearning/Trainer.cs
@@ -0,0 +1,33 @@
+﻿using BertMlNet.MachineLearning.DataModel;
+using Microsoft.ML;
+using System.Collections.Generic;
+
+namespace BertMlNet.MachineLearning
+{
+    public class Trainer
+    {
+        private readonly MLContext _mlContext;
+
+        public Trainer()
+        {
+            _mlContext = new MLContext(11);
+        }
+
+        public ITransformer BuidAndTrain(string bertModelPath, bool useGpu)
+        {
+            var pipeline = _mlContext.Transforms.ApplyOnnxModel(modelFile: bertModelPath,
+                                            outputColumnNames: new[] { "unstack:1",
+                                                                       "unstack:0",
+                                                                       "unique_ids:0" },
+                                            inputColumnNames: new[] {"unique_ids_raw_output___9:0",
+                                                                      "segment_ids:0",
+                                                                      "input_mask:0",
+                                                                      "input_ids:0" },
+                                            gpuDeviceId: useGpu ? 0 : (int?)null);
+
+            return pipeline.Fit(_mlContext.Data.LoadFromEnumerable(new List<BertInput>()));
+        }
+    }
+}
+//Severity	Code	Description	Project	File	Line	Suppression State
+//Error CS1061	'TransformsCatalog' does not contain a definition for 'ApplyOnnxModel' and no accessible extension method 'ApplyOnnxModel' accepting a first argument of type 'TransformsCatalog' could be found (are you missing a using directive or an assembly reference?)	
